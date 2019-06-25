9ec3a9339cd5323a26a171c34308f73ab94a9af7
SteShore <45996594+SteShore@users.noreply.github.com>
2019-06-25 12:55:33 +0100

    Add files via upload

---
+++ SmashCombo/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SmashCombo.ViewModel;
+
+namespace SmashCombo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new HomeVM();
+        }
+
+        private void MarioBtn_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new MarioVM();
+        }
+    }
+}
